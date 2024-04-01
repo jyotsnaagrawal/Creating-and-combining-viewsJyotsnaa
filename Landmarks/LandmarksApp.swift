de39166e66640dd57b0e694b35ab9aa806d35eeb
Agrawal, Jyotsna <122946262+jyotsnaagrawal@users.noreply.github.com>
2024-03-31 20:18:01 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by student on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
